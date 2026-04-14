bfc98dfe6aaabbe577cb165bf26b1213483ff6b5
TheMaximeSan <mxm.tournier@gmail.com>
2026-04-15 01:19:18 +0200

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,36 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ITyroUiNavbarPages, TyroUiButtom, TyroUiNavbar } from 'tyrolium-ui';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TyroUiButtom, TyroUiNavbar],
+  imports: [RouterOutlet, TyroUiButtom, TyroUiNavbar, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-
   /*
-  *
-  * GLOBAL VARIABLE
-  *
-  * */
+   *
+   * GLOBAL VARIABLE
+   *
+   * */
 
-  public APP_ENV = "DEV";
-  public PROJECT_NAME = "Tyrolium";
+  public APP_ENV = 'DEV';
+  public PROJECT_NAME = 'Tyrolium';
+  public PROJECT_LOGO = 'https://tyrolium.fr/Contenu/Image/Tyrolium%20Site.png';
 
   public pages: ITyroUiNavbarPages[] = [
-    { label: 'Accueil', link: '/' },
-    { label: 'Projet', link: '/project' },
-    { label: 'Prestation', link: '/prestation' }
-  ]
+    { label: 'Accueil', link: '/', icon: 'ri-home-line' },
+    { label: 'Projet', link: '/project', icon: 'ri-folder-line' },
+    {
+      label: 'Produit',
+      icon: 'ri-briefcase-line',
+      children: [
+        { label: 'Développement web', link: '/prestation/web', icon: 'ri-code-line' },
+        { label: 'Infrastructure', link: '/prestation/infra', icon: 'ri-server-line' },
+        { label: 'Conseil', link: '/prestation/conseil' }, // pas d'icône, optionnel
+      ],
+    },
+  ];
 }
