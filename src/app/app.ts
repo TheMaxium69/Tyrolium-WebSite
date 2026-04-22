a7d7171c9de9e4f3acb5fcc3a468fadb852cf7bb
maxime <mxm.tournier@gmail.com>
2026-04-22 15:00:35 +0200

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ITyroUiNavbarPages, TyroUiButtom, TyroUiFooter, TyroUiNavbar } from 'tyrolium-ui';
 import { Header } from './components/header/header';
