2d064a7dbb2fad9199a0f46cf4c2c5ec7b8beaaa
maxime <mxm.tournier@gmail.com>
2026-04-22 15:00:17 +0200

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -19,6 +19,17 @@ export class App {
   public APP_ENV = 'DEV';
   public PROJECT_NAME = 'Tyrolium';
   public PROJECT_LOGO = 'https://tyrolium.fr/Contenu/Image/Tyrolium%20Site.png';
+  public PROJECT_CONTENT = `<strong>TyroServ</strong>, un serveur <strong>Minecraft moddé</strong> depuis <strong>2017</strong>,
+                propose du <strong>PVP-Faction moddés</strong> (actuellement en saison 3) et bientôt des
+                <strong>mini-jeux moddés</strong>. <strong>Gratuit</strong> et semi-crack, il offre une sécurité
+                renforcée contre les tricheurs grâce à la création de comptes spécifiques.`
+
+
+  /*
+   *
+   * PAGES
+   *
+   * */
 
   public pages: ITyroUiNavbarPages[] = [
     { label: 'Accueil', link: '/', icon: 'ri-home-line' },
@@ -32,5 +43,19 @@ export class App {
         { label: 'Conseil', link: '/prestation/conseil' }, // pas d'icône, optionnel
       ],
     },
+  ];
+
+  public socials: ITyroUiNavbarPages[] = [
+    {label: 'facebook', link: 'https://www.facebook.com/tyrolium/', icon: 'ri-facebook-fill'},
+    {label: 'instagram', link: 'https://www.instagram.com/tyroliumentertainment/', icon: 'ri-instagram-line'},
+    {label: 'x', link: 'https://x.com/TyroliumE', icon: 'ri-twitter-x-fill'},
+    {label: 'youtube', link: 'https://www.youtube.com/@tyrolium', icon: 'ri-youtube-fill'},
+    {label: 'discord', link: 'https://discord.com/invite/km8h5jHezt', icon: 'ri-discord-fill'},
+    {label: 'linkedin', link: 'https://www.linkedin.com/company/tyrolium/', icon: 'ri-linkedin-fill'},
+    {label: 'tiktok', link: 'https://www.tiktok.com/@tyrolium', icon: 'ri-tiktok-fill'},
+    {label: 'twitch', link: 'https://www.twitch.tv/tyrolium', icon: 'ri-twitch-fill'},
+    {label: 'github', link: 'https://github.com/tyrolium', icon: 'ri-github-fill'},
+    {label: 'telegram', link: 'https://t.me/tyrolium', icon: 'ri-telegram-fill'},
+    {label: 'thread', link: 'https://www.threads.com/@tyroliumentertainment', icon: 'ri-threads-fill'},
   ];
 }
