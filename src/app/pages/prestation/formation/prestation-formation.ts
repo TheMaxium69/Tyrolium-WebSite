28b932f100c47fe39dd93d9c6bd17790b3bd50d5
TheMaximeSan <mxm.tournier@gmail.com>
2026-04-23 18:06:19 +0200

    formation final

--- src/app/pages/prestation/formation/prestation-formation.ts
+++ src/app/pages/prestation/formation/prestation-formation.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import {TyroUiCTA} from "tyrolium-ui";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-prestation-formation',
     templateUrl: './prestation-formation.html',
     styleUrls: ['../prestation-shared.css', './prestation-formation.css'],
     imports: [
-        TyroUiCTA
+        TyroUiCTA,
+        RouterLink
     ]
 })
-export class PrestationFormation {}
+export class PrestationFormation {
+    scrollToFormations() {
+        const element = document.getElementById('formations');
+        if (element) {
+            const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - 70;
+            window.scrollTo({top: offsetPosition, behavior: 'smooth'});
+        }
+    }
+}
