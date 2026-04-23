15beda0592e4b07ddc371ec3d026e229f2912415
TheMaximeSan <mxm.tournier@gmail.com>
2026-04-23 18:15:05 +0200

    final incubateur

--- src/app/pages/prestation/incubateur/prestation-incubateur.ts
+++ src/app/pages/prestation/incubateur/prestation-incubateur.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import {TyroUiCTA} from "tyrolium-ui";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-prestation-incubateur',
     templateUrl: './prestation-incubateur.html',
     styleUrls: ['../prestation-shared.css', './prestation-incubateur.css'],
     imports: [
-        TyroUiCTA
+        TyroUiCTA,
+        RouterLink
     ]
 })
-export class PrestationIncubateur {}
+export class PrestationIncubateur {
+    scrollToProgramme() {
+        const element = document.getElementById('programme');
+        if (element) {
+            const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - 70;
+            window.scrollTo({top: offsetPosition, behavior: 'smooth'});
+        }
+    }
+}
