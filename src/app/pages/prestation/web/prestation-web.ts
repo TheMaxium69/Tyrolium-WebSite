28cd9f35a9e709365e217646afeb16f5f586718a
TheMaximeSan <mxm.tournier@gmail.com>
2026-04-23 17:45:40 +0200

    page web final

--- src/app/pages/prestation/web/prestation-web.ts
+++ src/app/pages/prestation/web/prestation-web.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import {TyroUiCTA} from "tyrolium-ui";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-prestation-web',
     templateUrl: './prestation-web.html',
     styleUrls: ['../prestation-shared.css', './prestation-web.css'],
     imports: [
-        TyroUiCTA
+        TyroUiCTA,
+        RouterLink
     ]
 })
-export class PrestationWeb {}
+export class PrestationWeb {
+
+    scrollToOffres() {
+        const element = document.getElementById('offres');
+        if (element) {
+            const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - 70;
+            window.scrollTo({top: offsetPosition, behavior: 'smooth'});
+        }
+    }
+
+}
