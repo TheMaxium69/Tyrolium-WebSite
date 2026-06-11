0b8306ab19a9e742a5a38f756af0fc41a2fed4d3
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-11 03:13:07 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { TYRO_RELAY_URL, TYRO_RELAY_PAGE_URL } from 'tyrolium-ui';
 
 import { routes } from './app.routes';
 
@@ -12,6 +13,12 @@ export const appConfig: ApplicationConfig = {
           scrollPositionRestoration: 'enabled',
           anchorScrolling: 'enabled'
         })
-    )
+    ),
+    {
+      provide: TYRO_RELAY_URL,
+      useValue: isDevMode()
+        ? 'http://192.168.1.81:9001/relay.html'
+        : TYRO_RELAY_PAGE_URL,
+    },
   ],
 };
